54b5e8482e7180e810bfddaa384c8a1b4780cf3a
Shelby <sfholden3@gmail.com>
2020-03-10 20:23:09 -0400

    initial working version

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import json
 import requests
+import pandas as pd
 
 
-api_url_base = 'https://api-v3.mbta.com/'
+api_url_base = 'https://api-v3.mbta.com/routes?filter[type]=0,1'
 headers = {'Content-Type': 'application/json'}
 
 response = requests.get(api_url_base, headers=headers)
 
-print(response)
+class Routes:
+    def __init__(self, long_name, id):
+        self.long_name = long_name
+        self.id = id
+        
+    def q1(self):
+        print(self.long_name)
+        
+
+
+if response.status_code == 200:
+    print('Success!')
+    json_resp = response.json()
+    tests = json_resp["data"]
+    rts = []
+    for test in tests:
+        id = test['id']
+        name = test['attributes']['long_name']
+        rts.append(Routes(name, id))
+    df_routes = pd.DataFrame(rts)
+    for rt in rts:
+        rt.q1()
+elif response.status_code == 404:
+    print('Not Found.')
